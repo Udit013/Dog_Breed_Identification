2573dc03466c0f1998a23d8a600c8ea92a990f01
Udit013 <agarwaludit13@gmail.com>
2023-12-14 19:32:39 +0530

    initial commit

---
+++ test_train_split.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+labels = pd.read_csv('labels/labels.csv')
+
+train, val = train_test_split(labels, train_size=0.8, random_state=0)
+
+train.to_csv('labels/train.csv')
+val.to_csv('labels/val.csv')
